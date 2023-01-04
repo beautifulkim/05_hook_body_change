469097cbd2ecbb423f4b01d2326995039eb7adbb
martinkim <ga2d2000@nate.com>
2023-01-04 17:14:21 +0900

    토글 버튼 리엑트로 만듬

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  //userState로 선언된 변수의 값이 달라지면
+  //App 컴포넌트 재랜더링한다.
+  //open 초기값은 false로 설정.
+  const [open, setToggle] = useState(false);
+  console.log( open );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* 
+        버튼 클릭시 count의 값을 1로 변경해라 
+        count = count +1; count++
+      */}
+      <button className='btn' onClick={ () => { setToggle( !open ); } }>
+        { open ? '열기' : '닫기' }
+      </button>
+      <ul className={ open ? 'list_open' : 'list_close' }>
+        <li>테스트</li>
+        <li>테스트</li>
+        <li>테스트</li>
+      </ul>
     </div>
   );
 }
